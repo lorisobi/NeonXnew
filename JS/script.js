f8c11347706eec9ebb1f328a81d3b29b42efafe8
Jakub <66490771+alur2020@users.noreply.github.com>
2021-02-14 00:27:17 +0100

    Update 1.02
    Fixed bug (probably, last update didn't fix it)

--- JS/script.js
+++ JS/script.js
@@ -23,7 +23,7 @@ async function loadLangFile(file) {
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("serviceWorker.js")
+        .register("/NeonX/serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
